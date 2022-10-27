02c470e75bd49484a43e89f964dee9c6e4571752
peter-mourad <pmourad772@gmail.com>
2022-10-27 11:38:30 +0200

    registered Song model

--- albums/admin.py
+++ albums/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from albums.models import Album
+from albums.models import Album, Song
 
 class ShowAlbumsAdmin(admin.ModelAdmin):
     list_display = ('artist_name', 'name', 'released_at', 'cost', 'is_verified')
 
 admin.site.register(Album, ShowAlbumsAdmin)
+admin.site.register(Song)
