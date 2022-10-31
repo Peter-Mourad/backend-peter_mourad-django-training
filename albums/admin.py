e80f59fbcf1ea5d292dd828c141900c1ab00aa13
peter-mourad <pmourad772@gmail.com>
2022-10-31 13:59:54 +0200

    added songs admin

--- albums/admin.py
+++ albums/admin.py
@@ -5,5 +5,8 @@ from albums.models import Album, Song
 class ShowAlbumsAdmin(admin.ModelAdmin):
     list_display = ('artist_name', 'name', 'released_at', 'cost', 'is_verified')
 
+class ShowSongsAdmin(admin.ModelAdmin):
+    list_display = ['album', 'name']
+
 admin.site.register(Album, ShowAlbumsAdmin)
-admin.site.register(Song)
+admin.site.register(Song, ShowSongsAdmin)
