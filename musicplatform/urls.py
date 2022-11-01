627bafeaf8173780b8787810d1d4302375148fe2
peter-mourad <pmourad772@gmail.com>
2022-11-01 19:23:48 +0200

    added new urls

--- musicplatform/urls.py
+++ musicplatform/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('artists/', include('artists.urls')),
     path('albums/', include('albums.urls')),
+    path('authentication/', include('authentication.urls')),
+    path('users/', include('users.urls')),
+
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
