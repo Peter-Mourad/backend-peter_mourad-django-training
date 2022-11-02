1dac2e22473a6d9ec1e6adf94d9bf5a787007fb3
peter-mourad <pmourad772@gmail.com>
2022-11-02 12:52:13 +0200

    added password validating

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,4 +1,4 @@
-import email
+import email, re
 from rest_framework import serializers
 from users.models import User
 
@@ -8,13 +8,22 @@ class UserRegisterationSerializer(serializers.Serializer):
     password1 = serializers.CharField()
     password2 = serializers.CharField()
 
+    def validate_email(self, data):
+        print(data)
+        if User.objects.filter(email=email):
+            raise serializers.ValidationError("This email arleady exists")
+
+        return email
+
+    def validate_password1(self, password):
+        if re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{8,}', password):
+            return password
+
+        raise serializers.ValidationError("The password is weak!")
+
     def validate(self, data):
         if data.get('password1') != data.get('password2'):
             raise serializers.ValidationError("The passwords don't match")
+            
         return data
-
-    def validate_email(self, email):
-        if User.objects.filter(email=email):
-            raise serializers.ValidationError("This email arleady exists")
-        return email
 
